0460d5916911860c7c9a3a145ca4c760a6ad780a
Xiaodi Shi <xiaodi.shi@aces.su.se>
2024-08-22 18:46:14 +0200

    Update ConvertQI2MS.R

--- ConvertQI2MS.R
+++ ConvertQI2MS.R
@@ -1,11 +1,11 @@
-setwd("C:\\ACES\\R\\method development\\Suspect screening")
+setwd("C:\\ACES\\R\\method development\\Suspect screening") #Set the working directory
 
 #Dry condition
 {
-file_path <- "RealSed-dry-QI.txt"
+file_path <- "RealSed-dry-QI.txt" #Name of input file
 input_text <- paste(readLines(file_path, warn = FALSE), collapse = "\n")
 
-output_file_path <- "RealSed-dry-MS2.txt"
+output_file_path <- "RealSed-dry-MS2.txt" #Name of output file
 file_conn <- file(output_file_path, "w")
 spectra <- strsplit(input_text, "\n\n")[[1]]
 
@@ -13,6 +13,9 @@ for (spectrum in spectra) {
   lines <- strsplit(spectrum, "\n")[[1]]
   compound_name <- gsub("Name: ", "", lines[1])
   parent_mass <- as.numeric(gsub("PrecursorMZ: ", "", lines[3])) + 1.0078
+  
+  # Since SIRIUS only calculates the molecular compositions for protonated ions in positive mode, the mass of a hydrogen (i.e., 1.0078) is added to the mass of precursor ions for peaks detected in the dry condition. 
+  
   ionization <- gsub("Charge: ", "", lines[2])
   ms2_peaks_lines <- lines[6:length(lines)]
   ms2_peaks <- paste(ms2_peaks_lines, collapse = "\n")
@@ -32,10 +35,10 @@ close(file_conn)
 
 #Wet condition
 {
-  file_path <- "RealSed-wet-QI.txt"
+  file_path <- "RealSed-wet-QI.txt" #Name of input file
   input_text <- paste(readLines(file_path, warn = FALSE), collapse = "\n")
   
-  output_file_path <- "RealSed-wet-MS2.txt"
+  output_file_path <- "RealSed-wet-MS2.txt" #Name of output file
   file_conn <- file(output_file_path, "w")
   
   spectra <- strsplit(input_text, "\n\n")[[1]]
